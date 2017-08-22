ac7d9a2e583256e80ee08ec0bc7e71f4d441d8a9
Ubuntu <ubuntu@ip-172-31-36-217.us-west-2.compute.internal>
2017-08-22 23:32:07 +0000

    Aug22 1632

--- docker/postgres/initdb.sql
+++ docker/postgres/initdb.sql
@@ -1,3 +1,19 @@
-CREATE TABLE wiki_stuff (
-    id INTEGER
-);
+CREATE TABLE category (
+	id INTEGER,
+	name TEXT,
+	PRIMARY KEY (id));
+
+CREATE TABLE page (
+	page_id INTEGER,
+	title_id TEXT,
+	text TEXT);
+
+CREATE TABLE category_page(
+	page_id INTEGER,
+	category_id INTEGER,	
+	CONSTRAINT fk_category_id
+	FOREIGN KEY (category_id)
+	REFERENCES category(id),
+	CONSTRAINT fk_page_id
+	FOREIGN KEY (page_id)
+	REFERENCES page(id));
