9ac46af1e0a036f170366d552e11acd814f1521a
Ubuntu <ubuntu@ip-172-31-36-217.us-west-2.compute.internal>
2017-08-23 21:38:42 +0000

    Aug23 1438

--- docker/postgres/initdb.sql
+++ docker/postgres/initdb.sql
@@ -4,16 +4,19 @@ CREATE TABLE category (
 	PRIMARY KEY (id));
 
 CREATE TABLE page (
-	page_id INTEGER,
-	title_id TEXT,
+	id INTEGER,
+	title TEXT,
 	text TEXT);
 
 CREATE TABLE category_page(
 	page_id INTEGER,
 	category_id INTEGER,	
+
 	CONSTRAINT fk_category_id
 	FOREIGN KEY (category_id)
 	REFERENCES category(id),
+
 	CONSTRAINT fk_page_id
 	FOREIGN KEY (page_id)
 	REFERENCES page(id));
+
